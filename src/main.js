a8bac7bce6fb35d6477d13ad12cba1e58574a51c
Mikancat10 <mikancat09@gmail.com>
2026-01-24 03:23:38 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, "assets/icon.ico"),
     webPreferences: {
-      preload: __dirname + "/preload.js",
-      webviewTag: true,
+      preload: path.join(__dirname, "preload.js"),
+      webviewTag: true, 
       contextIsolation: true
     }
   });
